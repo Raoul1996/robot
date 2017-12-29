7eab77a79755753d9fd7673570e5af0c1ba291e1
Raoul1996 <baobaozhou199606@gmail.com>
2017-12-30 01:56:26 +0800

    Update Readme

--- config/config.default.js
+++ config/config.default.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('path')
+const fs = require('fs')
 module.exports = app => {
-  const exports = {};
+  const exports = {}
 
   exports.siteFile = {
     '/favicon.ico': fs.readFileSync(path.join(app.baseDir, 'app/web/asset/images/favicon.ico'))
-  };
+  }
 
   exports.logger = {
     consoleLevel: 'DEBUG',
     dir: path.join(app.baseDir, 'logs')
-  };
+  }
 
   exports.static = {
     prefix: '/public/',
     dir: path.join(app.baseDir, 'public')
-  };
+  }
 
-  exports.keys = '123456';
+  exports.keys = '123456'
 
   exports.middleware = [
     'access'
-  ];
+  ]
 
-  return exports;
-};
+  return exports
+}
