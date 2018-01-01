590665c087e39709f14d2dc7569bfb90a7495d7e
Raoul1996 <baobaozhou199606@gmail.com>
2018-01-01 16:50:16 +0800

    添加数据库 sql 文件

---
+++ seek/robot.sql
@@ -0,0 +1,76 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : localhost:3306
+ Source Schema         : robot
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : 65001
+
+ Date: 01/01/2018 16:47:25
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for competitions
+-- ----------------------------
+DROP TABLE IF EXISTS `competitions`;
+CREATE TABLE `competitions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `start_at` bigint(20) DEFAULT NULL,
+  `end_at` bigint(20) DEFAULT NULL,
+  `desc` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for records
+-- ----------------------------
+DROP TABLE IF EXISTS `records`;
+CREATE TABLE `records` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `comp_id` bigint(20) DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  `x` varchar(20) DEFAULT NULL,
+  `y` varchar(20) DEFAULT NULL,
+  `z` varchar(20) DEFAULT NULL,
+  `wx` varchar(20) DEFAULT NULL,
+  `wy` varchar(20) DEFAULT NULL,
+  `wz` varchar(20) DEFAULT NULL,
+  `v` varchar(20) DEFAULT NULL,
+  `a` varchar(20) DEFAULT NULL,
+  `created_at` varchar(20) DEFAULT NULL,
+  `updated_at` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `passsword` varchar(255) DEFAULT NULL,
+  `mobile` bigint(20) DEFAULT NULL,
+  `status` int(4) DEFAULT NULL,
+  `created_at` varchar(20) DEFAULT NULL,
+  `updated_at` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
